365ad68dcd7fd7c60d990272cc891d155d38c396
rifat-hossain <rifathossainayon@gmail.com>
2022-02-19 21:02:46 +0600

    resize fixed

--- SpriteSheetGen.py
+++ SpriteSheetGen.py
@@ -3,10 +3,11 @@ from os.path import isfile, join
 import cv2
 import numpy as np
 
-def insert_img(start_pos, sheet, img, hh, hw):
-    offset = [int((hh - img.shape[0])/2), int((hw - img.shape[1])/2)]
+def insert_img(start_pos, sheet, img, hh, hw, ratio):
+    img_resized = cv2.resize(img, (int(img.shape[1] * ratio),int(img.shape[0] * ratio)))
+    offset = [int((hh - img_resized.shape[0])/2), int((hw - img_resized.shape[1])/2)]
     start_pos = [start_pos[0] + offset[0], start_pos[1] + offset[1]]
-    sheet[start_pos[0]:start_pos[0]+img.shape[0],start_pos[1]:start_pos[1]+img.shape[1]] = img
+    sheet[start_pos[0]:start_pos[0]+img_resized.shape[0],start_pos[1]:start_pos[1]+img_resized.shape[1]] = img_resized
     return sheet
 direc = input("Enter directory: ")
 onlyfiles = [f for f in listdir(direc) if isfile(join(direc, f))]
@@ -35,12 +36,12 @@ try:
 except:
     spacing = 0
 ratio = sh / hh
-spacing = int(spacing / ratio)
+hh = int(ratio * hh)
+hw = int(ratio * hw)
 sw = (hw + (2 * spacing)) * len(onlyfiles)
 s_sheet = np.zeros((hh,sw,hc),dtype=float)
 for n in range(0,len(onlyfiles)):
-    s_sheet = insert_img([0,(n * hw) + ((2*n + 1) * spacing)], s_sheet, cv2.imread(direc + "/" + onlyfiles[n], cv2.IMREAD_UNCHANGED),hh,hw)
-resized = cv2.resize(s_sheet, (int(ratio * sw),sh))
+    s_sheet = insert_img([0,(n * hw) + ((2*n + 1) * spacing)], s_sheet, cv2.imread(direc + "/" + onlyfiles[n], cv2.IMREAD_UNCHANGED),hh,hw,ratio)
 if not path.exists(direc + "/ss/"):
     makedirs(direc + "/ss/")
-cv2.imwrite(direc + "/ss/spritesheet.png",resized)
+cv2.imwrite(direc + "/ss/spritesheet.png",s_sheet)
